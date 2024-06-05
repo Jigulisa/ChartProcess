9a237a964ebf76bd5f39c21261e334b776119ab2
Alya <145596958+Jigulisa@users.noreply.github.com>
2024-06-05 10:42:04 +0300

    Update main_file.py
    axises are labeled now

--- main_file.py
+++ main_file.py
@@ -19,25 +19,39 @@ class TheChart:
     def __init__(self, name, format, chart: Schedule):  # this class will determine
         self.name = name  # the size of image and save it
         self.format = format
-        self.sample = chart.points
+        self.chart = chart
         self.size = (600, 600)
+        self.im = Image.new('RGB', self.size, color='aliceblue')
+        self.draw = ImageDraw.Draw(self.im)
 
     def size_file(self):
-        weight = max(i[0] for i in self.sample) - min(i[0] for i in self.sample) + 40
-        height = max(i[1] for i in self.sample) - min(i[1] for i in self.sample) + 40
+        weight = max(i[0] for i in self.chart.points) - min(i[0] for i in self.chart.points) + 40
+        height = max(i[1] for i in self.chart.points) - min(i[1] for i in self.chart.points) + 40
         weight = 600 if weight < 600 else weight
         height = 600 if height < 600 else height
         self.size = (weight, height)
+        self.im.resize(self.size)
 
-    def create(self):
-        im = Image.new('RGB', self.size, color='aliceblue')
-        draw, left = ImageDraw.Draw(im), min(i[0] for i in self.sample)
-        but = min(i[1] for i in self.sample)
+    def create_axis(self):
+        left, but = min(i[0] for i in self.chart.points), min(i[1] for i in self.chart.points)
         but = -but if but < 0 else 0
         left = -left if left < 0 else 0
-        draw.line((20 + left, 20, 20 + left, self.size[1] - 20), fill=(0, 0, 0))
-        draw.line((20, self.size[1] - 20 - but, self.size[0] - 20, self.size[1] - 20 - but), fill=(0, 0, 0))
-        im.save(f'res.{self.format}')
+        self.draw.line((20 + left, 20, 20 + left, self.size[1] - 20), fill=(0, 0, 0))
+        self.draw.line((20, self.size[1] - 20 - but, self.size[0] - 20, self.size[1] - 20 - but), fill=(0, 0, 0))
+        self.draw.text((10 + left, 10), self.chart.y, fill=(0, 0, 0, 255))
+        self.draw.text((self.size[0] - 15, self.size[1] - 15 - but), self.chart.x, fill=(0, 0, 0, 255))
 
-# TODO 
-# 1 - interface; 2 - fix axises render; 3 - label axises
+    def draw_points(self):
+        weight = max(i[0] for i in self.chart.points) - min(i[0] for i in self.chart.points)
+        height = max(i[1] for i in self.chart.points) - min(i[1] for i in self.chart.points)
+        ed_otr = min(self.size[0] // weight, self.size[1] // height)
+        for i in self.chart.points:
+            self.draw.point((i[0] * ed_otr, i[1] * ed_otr), (0, 0, 0, 255))
+
+    def save(self):
+        self.im.save(f'res.{self.format}')
+
+# TODO
+# 1 - interface; 2 - fix axises render;
+
+
