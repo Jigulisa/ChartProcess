7119652f69bac1d734427c97f9727f9334912702
Alya <145596958+Jigulisa@users.noreply.github.com>
2024-06-08 12:58:07 +0300

    Update main_file.py
    added points sorting, and details

--- main_file.py
+++ main_file.py
@@ -2,10 +2,13 @@ from PIL import ImageDraw, Image
 
 
 class Schedule:
-    def __init__(self, x, y, *pairs):  # the class contains chart's points
+    def __init__(self, if_sort: bool, frt, name, x: str, y: str, *pairs):  # the class contains chart's points
         self.points = [*pairs]  # and names of its axises
         self.x = x
         self.y = y
+        self.name = name
+        self.frt = frt
+        self.sorting = if_sort
 
     def add_pairs(self, *pairs):
         self.points += pairs
@@ -16,13 +19,12 @@ class Schedule:
 
 
 class TheChart:
-    def __init__(self, name, format, chart: Schedule):  # this class will determine
-        self.name = name  # the size of image and save it
-        self.format = format
-        self.chart = chart
+    def __init__(self, chart: Schedule):  # this class will determine
+        self.chart = chart  # the size of image and save it
         self.size = (600, 600)
         self.im = Image.new('RGB', self.size, color='aliceblue')
         self.draw = ImageDraw.Draw(self.im)
+        self.center = (0, 0)
 
     def size_file(self):
         weight = max(i[0] for i in self.chart.points) - min(i[0] for i in self.chart.points) + 40
@@ -40,18 +42,37 @@ class TheChart:
         self.draw.line((20, self.size[1] - 20 - but, self.size[0] - 20, self.size[1] - 20 - but), fill=(0, 0, 0))
         self.draw.text((10 + left, 10), self.chart.y, fill=(0, 0, 0, 255))
         self.draw.text((self.size[0] - 15, self.size[1] - 15 - but), self.chart.x, fill=(0, 0, 0, 255))
+        self.center = (20 + left, self.size[1] - 20 - but)
+
+    def sort_points(self):
+        if self.chart.sorting:
+            self.chart.points = sorted(self.chart.points, key=lambda x: x[0])
 
     def draw_points(self):
         weight = max(i[0] for i in self.chart.points) - min(i[0] for i in self.chart.points)
         height = max(i[1] for i in self.chart.points) - min(i[1] for i in self.chart.points)
         ed_otr = min(self.size[0] // weight, self.size[1] // height)
-        for i in self.chart.points:
-            self.draw.point((i[0] * ed_otr, i[1] * ed_otr), (0, 0, 0, 255))
+
+        for i in range(len(self.chart.points) - 1):
+            m, v, c, d = (self.chart.points[i][0] * ed_otr,
+                          (self.size[1] - self.chart.points[i][1] * ed_otr),
+                          self.chart.points[i + 1][0] * ed_otr,
+                          (self.size[1] - self.chart.points[i + 1][1] * ed_otr))
+            self.draw.line((m, v, c, d), (0, 0, 0, 255))
 
     def save(self):
-        self.im.save(f'res.{self.format}')
+        self.im.save(f'{self.chart.name}.{self.chart.frt}')
+
 
 # TODO
-# 1 - interface; 2 - fix axises render; 3 - make current coordinates for cursor; 4 - label axises with numbers
+# 1 interface; 2 fix axises render; 3 make current coordinates for cursor; 4 label axises with numbers (optional)
 
 
+# example
+b = Schedule(True, 'jpg', 'schedule', 'x axis', 'y axis', (1, 2), (3, 4), (-1, 3), (4, 2))
+a = TheChart(b)
+a.size_file()
+a.create_axis()
+a.sort_points()
+a.draw_points()
+a.save()
